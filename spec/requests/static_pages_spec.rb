9bbae5dc2c7cd10bf64458ecc06708aed323d9d9
Victor Zhou <chezvz@gmai.com>
2013-11-17 01:58:33 -0600

    refactor spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,22 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) {"Ruby on Rails Tutorial Sample App | "}
+  ptitles = ["Home", "Help", "About Us", "Contact"]
+  pcontents = ["Sample App",  "Help", "About", "Contact"]
+  proutes = %w{home help about contact} 
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+  ptitles.zip(pcontents, proutes).each do |x|
+    describe "#{x[0]} page" do
+      it "should have the content '#{x[1]}'" do
+        visit "/static_pages/#{x[2]}"
+        expect(page).to have_content(x[1])
+      end
     end
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
-
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    it "should have the title '#{x[0]}'" do
+      visit "/static_pages/#{x[2]}"
+      expect(page).to have_title(base_title + x[0])
     end
   end
 end
